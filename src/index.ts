0854e314901c2e97feb614bd1aebf6253f225c6b
Ming <ximing.dai@linecorp.com>
2018-02-26 19:42:10 +0900

    init config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import sqlite3 from 'sqlite3'
+
+// https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
+
+export class APKG {
+  constructor(public options) {
+  }
+  addCard() {
+  }
+}
