7da70560047174772371ac7709a3e1e6a36a2f67
Ximing <netizendxm@gmail.com>
2018-03-01 06:07:42 +0900

    remove temp files

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,40 @@
 import * as Database from 'better-sqlite3'
 import { join } from 'path'
-import { genDatabaseSQL, genInsertionSQL } from './sql'
-import { writeFileSync, mkdirSync, rmdirSync, unlinkSync, createWriteStream } from 'fs'
+import { initDatabase, insertCard } from './sql'
+import { writeFileSync, mkdirSync, rmdirSync, createWriteStream } from 'fs'
+import * as rimraf from 'rimraf'
 import * as archiver from 'archiver'
 
 export class APKG {
   private db: any
   private deck: DeckConfig
+  private dest: string
   constructor(private config: DeckConfig) {
-    const dest = join(__dirname, config.name)
-    mkdirSync(dest)
-    writeFileSync(join(dest, 'media'), '{}')
-    this.db = new Database(join(dest, 'collection.anki2'))
+    this.dest = join(__dirname, config.name)
+    this.clean()
+    mkdirSync(this.dest)
+    writeFileSync(join(this.dest, 'media'), '{}')
+    this.db = new Database(join(this.dest, 'collection.anki2'))
     this.deck = {
       ...config,
       id: +new Date()
     }
-    const sql = genDatabaseSQL(this.deck)
-    this.db.exec(sql)
+    initDatabase(this.db, this.deck)
   }
   addCard(card: Card) {
-    const { SQL_CARD, SQL_NOTE } = genInsertionSQL(this.deck, card)
-    this.db.exec(SQL_CARD)
-    this.db.exec(SQL_NOTE)
+    insertCard(this.db, this.deck, card)
   }
   save(destination: string) {
     const directory = join(__dirname, this.config.name)
     const archive = archiver('zip')
     archive.directory(directory, false)
-    archive.pipe(createWriteStream(join(destination, `${this.config.name}.apkg`)))
+    archive.pipe(
+      createWriteStream(join(destination, `${this.config.name}.apkg`))
+    )
     archive.finalize()
-    archive.on('end', () => {
-      unlinkSync(join(directory, 'media'))
-      unlinkSync(join(directory, 'collection.anki2'))
-      rmdirSync(directory)
-    })
+    archive.on('end', this.clean.bind(this))
+  }
+  private clean() {
+    rimraf(this.dest, () => {})
   }
 }
-
