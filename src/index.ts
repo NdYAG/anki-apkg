fd79fddb3bd4a7236307a944f620f8dba80609b7
Richard Smith <richardmcsmith@gmail.com>
2018-11-14 10:52:07 +0900

    Add addMedia function

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,35 @@ export class APKG {
   private db: any
   private deck: DeckConfig
   private dest: string
+  private media: Array<string>
   constructor(private config: DeckConfig) {
     this.dest = join(__dirname, config.name)
     this.clean()
     mkdirSync(this.dest)
-    writeFileSync(join(this.dest, 'media'), '{}')
     this.db = new Database(join(this.dest, 'collection.anki2'))
     this.deck = {
       ...config,
       id: +new Date()
     }
     initDatabase(this.db, this.deck)
+    this.mediaFiles = []
   }
   addCard(card: Card) {
     insertCard(this.db, this.deck, card)
   }
+  addMedia(filename: string, data: Buffer) {
+    const index = this.mediaFiles.length
+    this.mediaFiles.push(filename)
+    writeFileSync(join(this.dest, `${index}`), data)
+  }
   save(destination: string) {
     const directory = join(__dirname, this.config.name)
     const archive = archiver('zip')
+    const mediaObj = this.mediaFiles.reduce((obj, file, idx) => {
+      obj[idx] = file
+      return obj
+    }, {})
+    writeFileSync(join(this.dest, 'media'), JSON.stringify(mediaObj))
     archive.directory(directory, false)
     archive.pipe(
       createWriteStream(join(destination, `${this.config.name}.apkg`))
